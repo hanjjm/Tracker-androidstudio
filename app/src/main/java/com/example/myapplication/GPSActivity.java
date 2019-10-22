aee0a5267c729f8dde64e69fe5d88539d64162a5
hanjjm <hanjjm1994@naver.com>
2019-10-23 04:01:13 +0900

    GPS Activity 3개의 fragment 구성

--- app/src/main/java/com/example/myapplication/GPSActivity.java
+++ app/src/main/java/com/example/myapplication/GPSActivity.java
@@ -1,42 +1,58 @@
 package com.example.myapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class GPSActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+public class GPSActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentTransaction transaction = fragmentManager.beginTransaction();
+    private HomeFragment homeFragment = new HomeFragment();
+    private GPSFragment gpsFragment = new GPSFragment();
+    private MyPageFragment myPageFragment = new MyPageFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
+    }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    transaction.replace(R.id.frameLayout, gpsFragment).commitAllowingStateLoss();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    transaction.replace(R.id.frameLayout, myPageFragment).commitAllowingStateLoss();
+                    break;
             }
-            return false;
+            return true;
         }
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gps);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+    public void onFragmentInteraction(Uri uri) {
 
+    }
 }
